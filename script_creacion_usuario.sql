c9de6a0fe063d93cd6f63f77da0b0d7df9fe37ae
Susan Castillo <susancasoli@gmail.com>
2024-04-08 22:24:59 -0600

    Creación del usuario terminada

---
+++ script_creacion_usuario.sql
@@ -0,0 +1,17 @@
+-- Iniciamos con nuestro usuario root
+mysql -u root
+
+-- Creamos el usuario y le otorgamos una contraseña
+CREATE USER 'db_sv_gol_desarrollador'@'localhost' IDENTIFIED BY '123456';
+ 
+-- Usamos nuestra base de datos
+USE db_gol_sv;
+ 
+-- Asignamos permisos para ver, actualizar, eliminar y crear
+GRANT SELECT, INSERT, UPDATE, DELETE ON db_gol_sv.* TO 'db_gol_sv_desarrollador'@'localhost';
+  
+-- Asignamos permisos para ejecutar y crear funciones, triggers, vistas y procedimientos
+GRANT EXECUTE, CREATE ROUTINE, CREATE VIEW, TRIGGER ON db_gol_sv.* TO 'db_gol_sv_desarrollador'@'localhost';
+  
+-- Mostramos los permisos que tiene asignado el desarrollador
+SHOW GRANTS FOR 'db_gol_sv_desarrollador'@'localhost';
