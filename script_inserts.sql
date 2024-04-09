c74c67a3b64eee6dd8473bcaa01104a72cb0b9ea
Susan Castillo <susancasoli@gmail.com>
2024-04-08 23:00:23 -0600

    Algunos inserts realizados

--- script_inserts.sql
+++ script_inserts.sql
@@ -56,22 +56,44 @@ USE db_gol_sv;
 
 
 -- 19. partidos
+INSERT INTO partidos (id_entrenamiento, id_equipo, logo_rival, rival_partido, fecha_partido, cancha_partido, resultado_partido, localidad_partido, tipo_resultado_partido)
 
 
--- 20. tipos_jugadores
-
+-- 20. tipos_jugadas
+INSERT INTO tipos_jugadas (id_tipo_jugada, nombre_tipo_juego) VALUES
+(1, 'Tiro libre'),
+(2, 'Juego directo'),
+(3, 'Juego combinativo'),
+(4, 'Contrataque'),
+(5, 'Sack'),
+(6, 'Intercepción'),
+(7, 'Touchdown'),
+(8, 'Field goal'),
+(9, 'Safety'),
+(10, 'Penalty'),
+(11, 'Fumble');
 
 -- 21. tipos_goles
-
+INSERT INTO tipos_goles (id_tipo_gol, id_tipo_jugada, nombre_tipo_gol) VALUES
+(1, 1, 'Tiro de esquina'),
+(2, 2, 'Dentro del area'),
+(3, 3, 'Pase corto'),
+(4, 4, 'Pase largo'),
+(5, 5, 'Carrera'),
+(6, 6, 'Touchdown'),
+(7, 7, 'Field goal'),
+(8, 8, 'Safety'),
+(9, 9, 'Penalty'),
+(10, 10, 'Fumble');
 
 -- 22. participaciones_partidos
-
+INSERT INTO participaciones_partidos (id_partido, id_jugador, titular, sustitucion, minutos_jugados, goles, asistencias, estado_animo, puntuacion)
 
 -- 23. detalles_goles
-
+INSERT INTO detalles_goles (id_participacion, cantidad_tipo_gol, id_tipo_gol)
 
 -- 24. detalles_amonestaciones
-
+INSERT INTO detalles_amonestaciones (id_participacion, amonestacion, numero_amonestacion)
 
 -- 25. tipos_lesiones
 
