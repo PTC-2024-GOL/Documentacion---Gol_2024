c86ff731094702cd27d0307ffc98f72ad5b3f4b7
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2024-04-09 14:20:09 -0600

    Se agrego la función

--- script_trigger_fun_proce.sql
+++ script_trigger_fun_proce.sql
@@ -18,5 +18,27 @@ DELIMITER ;
 
 -- FUNCION
 
+DELIMITER //
+
+CREATE FUNCTION generar_alias_administrador(nombre VARCHAR(50), apellido VARCHAR(50), fecha_creacion DATETIME) RETURNS VARCHAR(25)
+BEGIN
+    DECLARE alias_base VARCHAR(10);
+    DECLARE contador INT;
+    DECLARE alias_final VARCHAR(25);
+
+    SET alias_base = CONCAT(LEFT(nombre, 1), LEFT(apellido, 1), YEAR(fecha_creacion));
+
+    -- Encuentra el siguiente número disponible para el alias
+    SET contador = 1;
+    WHILE EXISTS (SELECT 1 FROM administradores WHERE alias_administrador = CONCAT(alias_base, contador)) DO
+        SET contador = contador + 1;
+    END WHILE;
+
+    -- Concatena el número al alias base para obtener el alias final
+    SET alias_final = CONCAT(alias_base, contador);
+    RETURN alias_final;
+END //
+
+DELIMITER ;
 
 -- PROCEDIMIENTO ALMACENADO
