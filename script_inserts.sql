38fb0125ffa4c3bad45dfb971eb625b87a74485f
Susan Castillo <susancasoli@gmail.com>
2024-04-08 22:41:42 -0600

    Script para insertar agregado

---
+++ script_inserts.sql
@@ -0,0 +1,91 @@
+-- SCRIPT DE INSERTS
+USE db_gol_sv;
+
+-- 1. Admin
+
+
+-- 2. tecnicos
+
+
+-- 3. temporadas
+
+
+-- 4. horarios
+
+
+-- 5. categorias
+
+
+-- 6. cuerpos_tecnicos
+
+
+-- 7. equipos
+
+
+-- 8. posiciones
+
+
+-- 9. jugadores
+
+
+-- 10. caracteristicas_jugadores
+
+
+-- 11. caracteristicas_analisis
+
+
+-- 12. asistencias
+
+
+-- 13. temas_contenidos
+
+
+-- 14. sub_temas_contenidos
+
+
+-- 15. tareas
+
+
+-- 16. detalles_contenidos
+
+
+-- 17. jornadas
+
+
+-- 18. entrenamientos
+
+
+-- 19. partidos
+
+
+-- 20. tipos_jugadores
+
+
+-- 21. tipos_goles
+
+
+-- 22. participaciones_partidos
+
+
+-- 23. detalles_goles
+
+
+-- 24. detalles_amonestaciones
+
+
+-- 25. tipos_lesiones
+
+
+-- 26. tipologias
+
+
+-- 27. sub_tipologias
+
+
+-- 28. lesiones
+
+
+-- 29. registro_medico
+
+
+-- 30. pago 
