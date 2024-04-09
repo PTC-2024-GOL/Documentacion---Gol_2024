de1288f63d8d823bcee05f933a26fff380ffc88e
Susan Castillo <susancasoli@gmail.com>
2024-04-08 23:40:03 -0600

    Trigger agregado

--- script_trigger_fun_proce.sql
+++ script_trigger_fun_proce.sql
@@ -3,7 +3,18 @@
 USE db_gol_sv;
 
 -- TRIGGER
-
+-- El trigger calcula el promedio de las notas de las subcaracterísticas para el jugador que ha sido modificado.
+DELIMITER //
+CREATE TRIGGER update_promedio_subcaracteristicas
+AFTER UPDATE ON caracteristicas_analisis
+FOR EACH ROW
+BEGIN
+  UPDATE vista_promedio_subcaracteristicas_por_jugador
+  SET promedio_subcaracteristicas = AVG(nota_caracteristica_analisis)
+  WHERE id_jugador = NEW.id_jugador;
+END;
+//
+DELIMITER ;
 
 -- FUNCION
 
