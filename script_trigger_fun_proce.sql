2fb0c9292c1f8a7afee21eddf97bbf849542adcd
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-04-11 11:01:21 -0600

    procedimiento almacenado listo

--- script_trigger_fun_proce.sql
+++ script_trigger_fun_proce.sql
@@ -44,8 +44,60 @@ END //
 DELIMITER ;
 
 -- PROCEDIMIENTO ALMACENADO
+DELIMITER //
 
+CREATE PROCEDURE insertar_o_actualizar_partido(
+    IN insertorUpdate INT,
+    IN id_entrenamiento_param INT,
+    IN id_equipo_param INT,
+    IN logo_rival_param VARCHAR(50),
+    IN rival_partido_param VARCHAR(50),
+    IN fecha_partido_param DATETIME,
+    IN cancha_partido_param VARCHAR(100),
+    IN resultado_partido_param VARCHAR(10),
+    IN localidad_partido_param ENUM('Local', 'Visitante'),
+    IN tipo_resultado_partido_param ENUM('Victoria', 'Empate', 'Derrota')
+)
+BEGIN
+    IF insertorUpdate = 0 THEN
+        INSERT INTO partidos (
+            id_entrenamiento, 
+            id_equipo, 
+            logo_rival, 
+            rival_partido, 
+            fecha_partido, 
+            cancha_partido, 
+            resultado_partido, 
+            localidad_partido, 
+            tipo_resultado_partido
+        ) 
+        VALUES (
+            id_entrenamiento_param, 
+            id_equipo_param, 
+            logo_rival_param, 
+            rival_partido_param, 
+            fecha_partido_param, 
+            cancha_partido_param, 
+            resultado_partido_param, 
+            localidad_partido_param, 
+            tipo_resultado_partido_param
+        );
+    ELSE
+        UPDATE partidos 
+        SET id_entrenamiento = id_entrenamiento_param, 
+            id_equipo = id_equipo_param, 
+            logo_rival = logo_rival_param, 
+            rival_partido = rival_partido_param, 
+            fecha_partido = fecha_partido_param, 
+            cancha_partido = cancha_partido_param, 
+            resultado_partido = resultado_partido_param, 
+            localidad_partido = localidad_partido_param, 
+            tipo_resultado_partido = tipo_resultado_partido_param
+        WHERE id_partido = insertorUpdate;
+    END IF;
+END //
 
+DELIMITER ;
 
 
 
