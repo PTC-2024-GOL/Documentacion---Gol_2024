1553d97cbcbdacd1620480ae7f317347a2a08ada
Susan Castillo <susancasoli@gmail.com>
2024-07-29 15:34:58 -0600

    Vista agregada en tecnicos

--- tecnicos - script_trigger_fun_proce.sql
+++ tecnicos - script_trigger_fun_proce.sql
@@ -19,3 +19,30 @@ INNER JOIN cuerpos_tecnicos ct ON e.id_cuerpo_tecnico = ct.id_cuerpo_tecnico
 INNER JOIN detalles_cuerpos_tecnicos dt ON ct.id_cuerpo_tecnico = dt.id_cuerpo_tecnico
 INNER JOIN tecnicos t ON dt.id_tecnico = t.id_tecnico
 INNER JOIN categorias c ON e.id_categoria = c.id_categoria;
+
+
+CREATE VIEW vista_detalle_partidos_tecnicos AS
+SELECT
+    p.id_partido,
+    DATE_FORMAT(p.fecha_partido, '%e de %M del %Y') AS fecha,
+    p.localidad_partido,
+    p.resultado_partido,
+    i.logo_rival,
+    e.logo_equipo,
+    e.nombre_equipo,
+    i.nombre_rival AS nombre_rival,
+    p.tipo_resultado_partido,
+    e.id_equipo,
+    d.id_tecnico
+FROM
+    partidos p
+INNER JOIN
+    equipos e ON p.id_equipo = e.id_equipo
+INNER JOIN
+	rivales i ON p.id_rival = i.id_rival
+INNER JOIN
+    detalles_cuerpos_tecnicos d ON e.id_cuerpo_tecnico = d.id_cuerpo_tecnico
+ORDER BY p.fecha_partido DESC;
+
+
+SELECT * FROM vista_detalle_partidos_tecnicos WHERE id_tecnico= 6;
